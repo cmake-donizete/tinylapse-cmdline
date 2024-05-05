8575be1a36c36401d54e9a8c30519acccccd8144
DonizeteVida <donizetevida1998@outlook.com>
2024-05-04 22:03:41 -0300

    Basic FFmpeg linking

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+It's simple: this project aims to generate a mp4 video 
+from a sequence of images
+
+# Clone
+
+# Compiling
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <libswresample/swresample.h>
+
+int main(int argc, char* argv[]) {
+    struct SwrContext* context = swr_alloc();
+    return EXIT_SUCCESS;
+}
